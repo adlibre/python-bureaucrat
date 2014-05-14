5de0fef45de96a7b3fe4bdcff7f0b7aa76f6def9
Andrew Cutler <macropin@gmail.com>
2014-05-14 11:10:15 +1000

    New version for initial PyPi release

--- setup.py
+++ setup.py
@@ -3,23 +3,37 @@
 import sys
 from setuptools import setup
 
+VERSION = '0.1.2'
 
 install_requires = []
 if sys.version_info < (2, 7):
     install_requires.append('argparse')
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("Warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
 setup(name='bureaucrat',
-    version='0.1.1',
+    version=VERSION,
     author="Andrew Cutler",
     author_email="andrew@adlibre.com.au",
     description="Procfile and Deployfile process manager for virtual environments",
     license="BSD",
-    long_description=open('README.md').read(),
+    long_description=read_md('README.md'),
     url='https://github.com/adlibre/python-bureaucrat',
+    download_url='https://github.com/adlibre/python-bureaucrat/archive/v%s.tar.gz' % VERSION,
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
     ],
     scripts=['bureaucrat'],
     install_requires=install_requires,
