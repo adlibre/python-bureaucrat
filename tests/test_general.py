91d48f32ff523737accef1dbd1ddafc457947627
Andrew Cutler <macropin@gmail.com>
2014-09-12 13:03:25 +1000

    Feature: Added support for limiting spawned tasks to PROCFILE_TASKS environment variable. Removed: Support for pid file writing. Improved unit test.

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,4 +1,3 @@
-import unittest
 import os
 
 from .bureaucrat import Bureaucrat
@@ -7,23 +6,47 @@ from .bureaucrat import Bureaucrat
 TESTS_PATH = os.path.abspath(os.path.split(__file__)[0])
 
 
-class BureaucratTest(unittest.TestCase):
+def test_start(capsys):
+    # setup
+    process_file = os.path.join(TESTS_PATH, 'data', 'Procfile')
+    env_file = os.path.join(TESTS_PATH, 'data', 'env')
+    virtual_env = '/tmp'
+    app_path = '/tmp'
+    log_path = '/tmp'
+    b = Bureaucrat(process_file,
+                   env_file,
+                   virtual_env,
+                   app_path,
+                   log_path,
+                   named_procs=None,
+                   debug=True)
 
-    def test_initialisation(self):
+    #
+    # Start test
+    #
+    b.start()
+    output, err = capsys.readouterr()
+    expected_output = 'Launching sleep0: sleep 0\n' \
+                      'Launching sleep5: sleep 5\n' \
+                      'Launching sleep10: sleep 10\n'
+    assert output == expected_output
 
-        process_file = os.path.join(TESTS_PATH, 'data', 'Procfile')
-        env_file = os.path.join(TESTS_PATH, 'data', 'env')
-        virtual_env = '/tmp'
-        app_path = '/tmp'
-        log_path = '/tmp'
-        pid_path = '/tmp'
-        named_procs = None
-        b = Bureaucrat(process_file,
-                       env_file,
-                       virtual_env,
-                       app_path,
-                       log_path,
-                       pid_path,
-                       named_procs,
-                       debug=True)
-        b.start()
+    # used for stop test
+    expected_output = ''
+    for p in b.process_manager.processes:
+        expected_output += 'Spawned process ended: %s (pid: %s exit: 0)\n' % \
+                           (p.name, p.sub_process.pid)
+
+    #
+    # Monitor test
+    #
+    b.monitor()
+
+    #
+    # stop test
+    #
+    capsys.close()
+    b.stop()
+    output, err = capsys.readouterr()
+    expected_output += 'All spawned processes have ended.\n'
+    assert output == expected_output
